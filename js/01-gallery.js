7613d112b394e7d02b1965996c90791ac85ba07f
kglanos <130902847+kglanos@users.noreply.github.com>
2023-09-08 22:32:44 +0200

    final

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,57 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const galleryContainer = document.querySelector(".gallery");
-let lightboxInstance = null;
+const galleryContainerElement = document.querySelector(".gallery");
+const imagesMarkup = createItemsMarkup(galleryItems);
+galleryContainerElement.insertAdjacentHTML("beforeend", imagesMarkup);
 
-function openModal(imageSrc, imageAlt) {
-    const content = `
-        <img src="${imageSrc}" alt="${imageAlt}" />`;
-
-    lightboxInstance = basicLightbox.create(content, {
-        onShow: () => {
-            document.addEventListener('keydown', handleEscapeKey);
-        },
-        onClose: () => {
-            document.removeEventListener('keydown', handleEscapeKey);
-        }
-    });
-
-    lightboxInstance.show();
+function createItemsMarkup(item) {
+    return galleryItems
+    .map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+            <a class="gallery__link" href="${original.value}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+        </a>
+    </div>`;
+    })
+    .join("");
 }
+const onContainerClick = (e) => {
+    e.preventDefault();
 
-function handleEscapeKey(event) {
-    if (event.key === 'Escape') {
-        lightboxInstance.close();
-    }
-}
+    if (e.target.classList.contains("gallery")) return;
+    const source = e.target.dataset.source;
+    
+    const instance = basicLightbox.create(`
+    <img src="${source}"width="800" height="600">`);
 
-function addGalleryItems() {
-    galleryItems.forEach((item) => {
-        const listItem = document.createElement("li");
-        listItem.classList.add("gallery__item");
+    instance.show();
+};
 
-        const link = document.createElement("a");
-        link.classList.add("gallery__link");
-        link.href = "#";
-
-        const image = document.createElement("img");
-        image.classList.add("gallery__image");
-        image.src = item.preview;
-        image.alt = item.description;
-        image.dataset.source = item.original;
-
-        link.addEventListener("click", (e) => {
-            e.preventDefault();
-            openModal(item.original, item.description);
-        });
-
-        link.appendChild(image);
-        listItem.appendChild(link);
-        galleryContainer.appendChild(listItem);
-    });
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    addGalleryItems();
-});
+galleryContainerElement.addEventListener("click", onContainerClick);
---
+++ readme.md
@@ -0,0 +1,2 @@
+https://github.com/kglanos/goit-js-hw-07
+https://kglanos.github.io/goit-js-hw-07/index.html
